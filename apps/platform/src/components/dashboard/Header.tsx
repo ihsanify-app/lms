1d918d871ace927c716384ec79b959234dbd3a9b
afsuyadi <afsuyadi@gmail.com>
2026-04-28 15:44:15 +0700

    feat(components): add Header

---
+++ apps/platform/src/components/dashboard/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+	return (
+		<header className="flex flex-row">
+			<h1>Dashboard</h1>
+			<div>
+				<span>🔔</span>
+				<span>👤 Admin</span>
+			</div>
+		</header>
+	);
+}
